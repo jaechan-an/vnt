04a0ec457514ea2830d072a7d9be85dc15d6c383
Jaechan An <jaechan@umd.edu>
2025-07-08 04:02:22 -0400

    Script fix

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 rm -rf exp
-rm -rf logs
-rm -rf receipts
-
 mkdir -p exp
-
-./reset_db.sh
 
 NUM_TABLES=4
 NUM_RECORDS_ARRAY=(50 100 500 1000 2000 3000 4000 5000)
 
 for NUM_RECORDS in "${NUM_RECORDS_ARRAY[@]}"; do
   echo "Running simulation with ${NUM_TABLES} tables and ${NUM_RECORDS} records..."
+
+  ./reset_db.sh
+
+  rm -rf logs
+  rm -rf receipts
 
   cargo run --bin simulator -- --tables=${NUM_TABLES} --records=${NUM_RECORDS}
 
