ced6d955eb561ddbff976d7c16ef3140eecdb3e7
Jaechan An <jaechan@umd.edu>
2025-10-28 22:55:25 -0400

    Final version which ran the experiment.

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -4,6 +4,7 @@ use hex;
 use risc0_zkvm::guest::env;
 use sha2;
 use sha2::Digest;
+use std::collections::HashMap;
 use std::convert::TryInto;
 
 fn main() {
@@ -45,24 +46,11 @@ fn main() {
 
     if !prev_tree.is_empty() {
         let prev_root = prev_tree.root();
-        println!(
-            "Prev Merkle tree root: {}",
-            hex::encode(prev_root.as_bytes())
-        );
-        println!("Prev Merkle tree entries:");
-        for (idx, clog) in prev_tree.elements().iter().enumerate() {
-            println!("\tIndex: {}, clog: {}", idx + 1, clog.to_string());
-        }
 
         for (flow_id, _clog) in &input.update_clogs {
             if let Some(old_clog) = input.old_clogs.get(flow_id) {
                 let idx = util::id_to_idx(old_clog.id);
                 let proof = prev_tree.prove(idx);
-                println!(
-                    "Index to prove: {}, clog: {}",
-                    old_clog.id,
-                    old_clog.to_string()
-                );
                 assert_eq!(idx, proof.index());
                 assert!(proof.verify(&prev_root, old_clog));
             }
@@ -71,6 +59,33 @@ fn main() {
 
     end = env::cycle_count();
     println!("Old clogs verified in {} cycles", end - start);
+
+    // Check if the new clogs are a match of the aggregation
+    let diff_clogs = aggregate_logs(&input.new_logs);
+
+    for (flow_id, clog) in &diff_clogs {
+        let mut input_clog = input.update_clogs.get(flow_id);
+
+        if input_clog.is_none() {
+            input_clog = input.insert_clogs.get(flow_id);
+        }
+
+        assert!(
+            input_clog.is_some(),
+            "No matching clog found for flow_id {}",
+            flow_id
+        );
+
+        assert!(
+            clog.flow_id == input_clog.unwrap().flow_id
+                && clog.src == input_clog.unwrap().src
+                && clog.dst == input_clog.unwrap().dst
+                && clog.packet_size == input_clog.unwrap().packet_size
+                && clog.hop_cnt == input_clog.unwrap().hop_cnt,
+            "Aggregated clog mismatch for flow_id {}",
+            flow_id
+        );
+    }
 
     // Step 3. Now update the Merkle tree with update_clogs and insert_clogs.
     // The result will be a new Merkle tree with the updated logs. This Merkle tree will
@@ -109,39 +124,20 @@ fn main() {
     sorted_inserts.sort_by_key(|(_, clog)| clog.id);
     for (flow_id, clog) in sorted_inserts {
         assert!(input.old_clogs.get(flow_id).is_none());
-        println!("{} for {}, {}", clog.id, clog.flow_id, clog.hop_cnt);
 
         elements.push(clog.clone());
-        println!(
-            "Index Inserted: {}, clog: {}",
-            clog.id,
-            clog.to_string()
-        );
     }
-
-    println!("New elements {}", elements.len());
 
     // Build the new Merkle tree from the updated elements
     let new_tree = MerkleTree::new(elements);
 
     let new_root = new_tree.root();
-    println!(
-        "New Merkle tree root: {}",
-        hex::encode(new_root.as_bytes())
-    );
+    println!("New Merkle tree root: {}", hex::encode(new_root.as_bytes()));
 
     end = env::cycle_count();
     println!("New Merkle tree built in {} cycles", end - start);
     println!("Merkle tree depth: {}", new_tree.depth());
-    println!(
-        "New merkle tree has {} leaves.",
-        new_tree.elements().len()
-    );
-
-    println!("New Merkle tree elements:");
-    for (idx, clog) in new_tree.elements().iter().enumerate() {
-        println!("\tIndex: {}, clog: {}", idx + 1, clog.to_string());
-    }
+    println!("New merkle tree has {} leaves.", new_tree.elements().len());
 
     // Step 4. Output the journal with the success status, Merkle tree, and root.
     start = env::cycle_count();
@@ -178,4 +174,26 @@ fn compute_hash(logs: &Vec<Log>) -> [u8; 32] {
     }
 
     hasher.finalize().into()
+}
+
+/**
+ * Aggregates logs from all nodes into a single HashMap where the key is the flow_id.
+ * The CLog struct is used to represent the aggregated log.
+ */
+fn aggregate_logs(new_logs: &Vec<Vec<Log>>) -> HashMap<i32 /* flow_id */, CLog> {
+    let mut aggregated_map = HashMap::<i32, CLog>::new();
+
+    for logs in new_logs {
+        for log in logs {
+            let key = log.flow_id;
+
+            // If key exists, modify it; otherwise, insert a new value
+            aggregated_map
+                .entry(key)
+                .and_modify(|clog: &mut CLog| clog.hop_cnt += log.hop_cnt)
+                .or_insert(CLog::from_log(&log));
+        }
+    }
+
+    aggregated_map
 }
